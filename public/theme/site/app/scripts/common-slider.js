a3fab8b74adeee8a9ba7ac1ebecb3358ee124936
etomarat <etomarat@gmail.com>
2015-04-16 19:35:39 +0300

    news markup

--- public/theme/site/app/scripts/common-slider.js
+++ public/theme/site/app/scripts/common-slider.js
@@ -1,53 +1,62 @@
 
-$(document).ready(function() {
-  $('.common-slider').each(function(){
-    
-    function prerender(newIndex) {
-      if (newIndex==0) {
-        $prev.css({
-          visibility: 'hidden'
-        });
-      } else {
-        $prev.css({
-          visibility: 'visible'
-        });
-      };
-      
-      if (newIndex==_count) {
-        $next.css({
-          visibility: 'hidden'
-        });
-      } else {
-        $next.css({
-          visibility: 'visible'
-        });
-      }
-      $numbers.text((newIndex+1)+'/'+(_count+1))
-    }
-    
-    var _count = $(this).find('>li').size()-1;
-    var $slider = $(this).bxSlider({
-      pager: false,
-      infiniteLoop: false,
-      controls: false,
-      onSliderLoad: function($el, currentIndex){
-        prerender(currentIndex);
-      },
-      onSlideBefore: function($slideElement, oldIndex, newIndex){
-        prerender(newIndex);
-      }
-    });
-    var $next = $(this).closest('.bx-wrapper').prev('.common-slider-controls').find('.next');
-    var $prev = $(this).closest('.bx-wrapper').prev('.common-slider-controls').find('.prev');
-    var $numbers = $(this).closest('.bx-wrapper').prev('.common-slider-controls').find('.numbers');
+var _AGROKOM_render_slider;
 
-    $next.click(function(e){
-      e.preventDefault();
-      $slider.goToNextSlide();
+$(document).ready(function() {
+  _AGROKOM_render_slider = function() {
+    $('.common-slider').each(function(){
+      if (!$(this).data('slder')) {
+        var $next = $(this).prev('.common-slider-controls').find('.next');
+        var $prev = $(this).prev('.common-slider-controls').find('.prev');
+        var $numbers = $(this).prev('.common-slider-controls').find('.numbers');
+        
+        var _count = $(this).find('>li').size()-1;
+        var $slider = $(this).bxSlider({
+          pager: false,
+          infiniteLoop: false,
+          controls: false,
+          onSliderLoad: function($el, currentIndex){
+            prerender(currentIndex);
+          },
+          onSlideBefore: function($slideElement, oldIndex, newIndex){
+            prerender(newIndex);
+          }
+        });
+        $(this).data('slider', $slider);
+        
+        $next.click(function(e){
+          e.preventDefault();
+          $slider.goToNextSlide();
+        });
+        $prev.click(function(e){
+          e.preventDefault();
+          $slider.goToPrevSlide();
+        });
+        
+        function prerender(newIndex) {
+          if (newIndex==0) {
+            $prev.css({
+              visibility: 'hidden'
+            });
+          } else {
+            $prev.css({
+              visibility: 'visible'
+            });
+          };
+          
+          if (newIndex==_count) {
+            $next.css({
+              visibility: 'hidden'
+            });
+          } else {
+            $next.css({
+              visibility: 'visible'
+            });
+          }
+          $numbers.text((newIndex+1)+'/'+(_count+1))
+        }
+
+      }
     });
-    $prev.click(function(e){
-      e.preventDefault();
-      $slider.goToPrevSlide();
-    });
-  });
+  }
+  _AGROKOM_render_slider();
 });
--- public/theme/site/app/scripts/main.js
+++ public/theme/site/app/scripts/main.js
@@ -1,2 +1,10 @@
 /* jshint devel:true */
 //console.log('\'Allo \'Allo!');
+$(document).ready(function() {
+  $('.news-list').jscroll({
+    contentSelector: '.news-list .unit',
+    callback: function(){
+      _AGROKOM_render_slider();
+    }
+  });
+})
