57d007c2193cd0883da993069655c7d28d59cc33
etomarat <etomarat@gmail.com>
2015-04-20 17:37:45 +0300

    many markup

---
+++ public/theme/site/app/scripts/main-slider.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+  var $slider = $('#main-slider'),
+      $slides = $slider.find('.slide'),
+      $dots = $slider.find('.dots a'),
+      delay = 5*1000,
+      timer;
+      
+  $dots.click(function(e){
+    e.preventDefault();
+    if (!$(this).hasClass('active')) {
+      var count = $dots.size()-1;
+      var cur = $dots.index($(this));
+      
+      $slides.removeClass('active');
+      $dots.removeClass('active');
+      setTimeout(function(){
+        $slides.eq(cur).addClass('active');  
+      }, 100);
+      $(this).addClass('active');
+      
+      if (cur>=count) {
+        var next = 0;
+      } else {
+        var next = cur+1;
+      }
+      clearTimeout(timer);
+      timer = setTimeout(function(){
+        $dots.eq(next).click();
+      }, delay);
+      
+    }
+  });
+  
+  $(window).load(function() {
+    $dots.eq(0).click();
+  });
+});
