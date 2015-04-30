79e4c8f52e263129bd3afd8ba2781ba3491ac121
sngrl <reserved@mail.ru>
2015-04-30 18:44:13 +0300

    fix

---
+++ app/views/templates/site/about.blade.php
@@ -0,0 +1,39 @@
+<?
+/**
+ * TITLE: О компании
+ * AVAILABLE_ONLY_IN_ADVANCED_MODE
+ */
+?>
+@extends(Helper::layout())
+
+
+@section('header')
+    @include(Helper::layout('header_about'))
+@stop
+
+
+@section('body_class') business @stop
+
+
+@section('content')
+
+    <div class="slogan">
+        {{ nl2br(trans("interface.slogan")) }}
+    </div>
+    <div id="preload-visual" data-img="{{ Config::get('site.theme_path') }}/images/visual-about.jpg" class="visual-wide">
+        <div class="holder">
+            <div class="teaser"><img src="{{ Config::get('site.theme_path') }}/images/visual-about-text.png" style="margin-bottom: -110px;margin-left: 75px;"></div>
+        </div>
+    </div>
+    <div class="content">
+        <h1>
+            {{ $page->h1_or_name() }}
+        </h1>
+    </div>
+    {{ $page->block('content') }}
+
+@stop
+
+
+@section('scripts')
+@stop
