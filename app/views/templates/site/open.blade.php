309a2f6b198d611cd5b57540b2248c4ff9e24e13
sngrl <reserved@mail.ru>
2015-05-19 12:53:45 +0300

    fix

---
+++ app/views/templates/site/open.blade.php
@@ -0,0 +1,50 @@
+<?
+/**
+ * TITLE: Раскрытие информации
+ * AVAILABLE_ONLY_IN_ADVANCED_MODE
+ */
+?>
+@extends(Helper::layout())
+<?
+if (isset($page) && is_object($page))
+    $seo = $page->seo;
+?>
+
+
+@section('body_class') contacts @stop
+
+
+@section('content')
+
+    <div class="sub-header">
+
+        {{ Menu::placement('news_press') }}
+
+    </div>
+
+    <div class="content">
+        <div class="row">
+            <div class="address">
+
+                <h1 class="title">
+                    {{ $page->h1_or_name() }}
+                </h1>
+
+                @if (count($page->blocks))
+                    @foreach ($page->blocks as $block)
+                        @if (0)
+                            <h2>{{ $page->block($block->slug, 'name') }}</h2>
+                        @endif
+                        {{ $page->block($block->slug) }}
+                    @endforeach
+                @endif
+
+            </div>
+        </div>
+    </div>
+
+@stop
+
+
+@section('scripts')
+@stop
