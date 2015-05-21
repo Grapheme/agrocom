b85a346f2097eb89161ac3aeb32fd21aa7095b03
sngrl <reserved@mail.ru>
2015-05-21 12:59:52 +0300

    fix

--- app/views/templates/site/contacts.blade.php
+++ app/views/templates/site/contacts.blade.php
@@ -66,5 +66,9 @@
     <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
     <script>
         var _AGROKOM_coords = [{{ $page->block('coords') }}];
+        var _AGROKOM_ = {
+            'required': 'Обязательное поле!',
+            'email_format': 'Неверный формат. Попробуйте еще!'
+        };
     </script>
 @stop
