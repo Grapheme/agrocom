dfb6a488dd3f6f991d753c20101e0b98cba4cde5
grphm-robot <info@grapheme.ru>
2015-06-10 14:44:53 +0300

    server commit

--- app/views/templates/site/scripts.blade.php
+++ app/views/templates/site/scripts.blade.php
@@ -6,3 +6,14 @@
 
     {{ HTML::scriptmod(Config::get('site.theme_path').'/scripts/vendor.js') }}
     {{ HTML::scriptmod(Config::get('site.theme_path').'/scripts/main.js') }}
+    
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-17193616-18', 'auto');
+  ga('send', 'pageview');
+
+</script>
