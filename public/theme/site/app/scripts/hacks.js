4f7750c80608a0dd40ca5a6cc51047a941de4513
etomarat <etomarat@gmail.com>
2015-06-19 14:43:05 +0300

    2 col

---
+++ public/theme/site/app/scripts/hacks.js
@@ -0,0 +1,41 @@
+var arr_p = [];
+$(document).ready(function() {
+  
+  /* this make all text into div */
+  $('body.news-detail .content').contents().filter(function(){
+    return this.nodeType !== 1;
+  })
+  .wrap( "<p></p>" );
+  
+  /* this delete empty <p> */
+  $('body.news-detail .content p').each(function(){
+    if ($(this).text().replace(/\s+/g, '')==''){
+      $(this).remove();
+    }
+  });
+  
+  //$('body.news-detail .content p').wrapAll( "<div class='new' />");
+  $('body.news-detail .content p').each(function(){
+    if (!$(this).prev().is('p')) {
+      arr_p.push([$(this)]);
+    } else {
+      arr_p[arr_p.length-1].push($(this));
+    }
+  });
+  
+  /*$.each(arr_p, function(index,value){
+    var $_this = $();
+    $.each(value, function(index2, value2){
+      $_this.add(value2);
+    })
+    $_this.wrap('<div class="new" />');
+  });*/
+  
+  $('body.news-detail .content p').map(function() {
+    if (!$(this).prev().is('p')) {
+        return $(this).nextUntil(':not(p)').andSelf();
+    }
+  }).wrap("<div class='text-col-2' />");
+  
+  console.log(arr_p);
+})
--- public/theme/site/app/scripts/visual.js
+++ public/theme/site/app/scripts/visual.js
@@ -12,11 +12,12 @@ $(document).ready(function() {
     $('#main-slider .preloader-wrapper .preloader').removeClass('loaded');
     setTimeout(function(){
       $('header .holder').addClass('loaded');
-      $('.sub-header').addClass('loaded');
       $('#main-slider').addClass('loaded');
       $('.slogan').addClass('loaded');
     }, 100)
   });
+  
+  $('.sub-header').addClass('loaded');
   
   function preloadImage(elem) {
     elem = $(elem);
